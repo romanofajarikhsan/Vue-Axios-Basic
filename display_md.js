cd32bd2d6fe6c1bd32d228bd62987d0e6452152b
Farid <farid.suryanto@is.uad.ac.id>
2022-04-13 09:56:30 +0700

    update

---
+++ display_md.js
@@ -0,0 +1,31 @@
+Vue.createApp({
+    data() {
+      return {       
+        article: null,
+      };
+    },
+    methods: {
+      getMarkdownData()
+      {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const article = urlParams.get('article');        
+        var converter = new showdown.Converter();
+        axios
+          .get(
+            "https://raw.githubusercontent.com/faridsurya/Vue-Axios-Basic/master/contents/"+article
+          )
+          .then((res) => {           
+            var html = converter.makeHtml(res.data);           
+            this.article = html;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    beforeMount() {
+      this.getMarkdownData()
+    },
+  }).mount("#app");
+  
